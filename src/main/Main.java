507f9ade6d65dba30d26cb5f5042e705d89f5684
sparshgupta2212 <sparsh.gupta_cs18@gla.ac.in>
2019-08-22 15:00:24 +0530

    add file Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,15 +1,7 @@
 package main;
 
-import java.util.Scanner;
-
 /**
  * @author sparsh gupta
  * @version 1.0
  */
-public class Main {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Welcome,""!");
-    }
-}
+public class Main
