c5dbea6c0b844b2d487e8477b3c2b07c1edcfa09
sparshgupta2212 <sparsh.gupta_cs18@gla.ac.in>
2019-08-22 14:57:42 +0530

    init commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,8 +9,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String userName = input.nextLine().trim();
-        System.out.println("Welcome, " + userName + "!");
-        input.close();
+
+        System.out.println("Welcome,""!");
     }
 }
